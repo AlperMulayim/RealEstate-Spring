207a56e847a20e47c31f74132fc3bea51e8e2dfb
AlperMulayim <alpermulayim@gtu.edu.tr>
2018-03-23 21:31:26 +0300

    The Broker add , This Broker name should set on the pub sub page

---
+++ broker/src/main/java/com/example/demo/BrokerApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner listen(PubSubTemplate pubSubTemplate){
+		return args->{
+			pubSubTemplate.subscribe("newHouses-broker1",
+					(message ,consumer)->{
+						System.out.println("New House For Sale " + message.getData().toStringUtf8());
+					});
+		};
+	}
+}
