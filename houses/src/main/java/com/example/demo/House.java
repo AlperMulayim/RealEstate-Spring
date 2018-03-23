696d8d315865fb25d7204fb0b2dac54e7e264169
AlperMulayim <alpermulayim@gtu.edu.tr>
2018-03-23 18:05:31 +0300

    House Entity created

---
+++ houses/src/main/java/com/example/demo/House.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by Alper on 23.03.2018.
+ */
+@Entity
+public class House {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String address;
+
+    public House(long id, String address) {
+        this.id = id;
+        this.address = address;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ houses/src/main/java/com/example/demo/HouseRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Alper on 23.03.2018.
+ */
+public interface HouseRepository extends CrudRepository<House , Long> {
+
+}
