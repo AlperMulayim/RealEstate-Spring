d8f23863e16b9ce74c094fb27d8857309bee45ba
AlperMulayim <alpermulayim@gtu.edu.tr>
2018-04-25 14:24:30 +0300

    Task implemented

--- TaskManager/tasks/src/main/java/com/alper/domain/Task.java
+++ TaskManager/tasks/src/main/java/com/alper/domain/Task.java
@@ -1,7 +1,27 @@
 package com.alper.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
 /**
  * Created by Alper on 25.04.2018.
  */
+@Entity
+@Data
+@AllArgsConstructor
 public class Task {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    @JsonFormat(pattern = "MM/dd/yyyy")
+    private LocalDate dueDate;
+    private boolean complated;
 }
